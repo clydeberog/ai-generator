49951dd2b12418839d5574a7bc72d9b3a72641ea
Francis Clyde Laguda Berog <122136728+clydeberog@users.noreply.github.com>
2023-04-05 07:09:07 +0800

    update links

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -2,9 +2,12 @@ import mongoose from 'mongoose';
 
 const connectDB = (url) => {
   mongoose.set('strictQuery', true);
-  mongoose.connect("mongodb+srv://clydefrancis2:clydeclyde3995@cluster0.ivgtlcm.mongodb.net/?retryWrites=true&w=majority")
-    .then(() => console.log('Connected to MongoDB cluster'))
-    .catch((err) => console.log(err));
+  mongoose.connect("mongodb+srv://clydefrancis:clydeclyde3995@cluster0.ivgtlcm.mongodb.net/?retryWrites=true&w=majority")
+    .then(() => console.log('Connected to MongoDB Cluster'))
+    .catch((err) => {
+      console.error('Failed to Connect');
+      console.error(err);
+    });
 };
 
 export default connectDB;
